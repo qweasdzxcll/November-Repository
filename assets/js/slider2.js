e15055be1c947c08b596aaba806fee80b7820879
qweasdzxcll <denispodmogilnyj@gmail.com>
2024-11-17 12:49:02 +0300

    first commit

---
+++ assets/js/slider2.js
@@ -0,0 +1,25 @@
+let offset = 0;
+const sliderLine= document.querySelector('.main__line');
+
+// setInterval(() => {
+//     nextSlide()
+// }, 3000);
+
+function nextSlide() {
+    offset += 1920;
+    if (offset >= 3840) {
+        offset = 0
+    }
+    sliderLine.style.left = -offset + 'px';
+}
+function prevSlide() {
+    offset -= 1920;
+    if (offset < 0) {
+        offset = 1920;
+    }
+    sliderLine.style.left = -offset + 'px'
+}
+
+document.querySelector('.btns__btn-next').onclick = nextSlide
+
+document.querySelector('.btns__btn-prev').onclick = prevSlide
